8442b1d118707a5b6f3f44e4ed87e4888f7a9631
aluno <etecae023@hotmail.com>
2025-09-15 11:28:13 -0300

    interface grafica

---
+++ src/InterfaceGrafica.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class InterfaceGrafica {
+    private JTextField autoformaçãoSTEAMIATextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste github!");
+        JFrame frame = new JFrame("Teste");
+        frame.setSize(500,300);
+        JPanel painel = new JPanel();
+        painel.setSize(300,200);
+        painel.setLayout(new GridLayout());
+        JLabel label = new JLabel();
+        label.setText("Autoformação\nSTEAM + IA");
+        painel.add(label);
+        frame.add(painel);
+        frame.setVisible(true);
     }
 }
